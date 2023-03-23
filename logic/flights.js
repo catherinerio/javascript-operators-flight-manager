cfb309b9e47ef3c2804a08b465b6660b340f2d0e
Catherine <p.catherinerio@gmail.com>
2023-03-23 22:26:47 +1100

    Update flights.js

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,61 @@
 function Flights() {
-
+   function calculateNumberOfFlights(numpass, flightcapacity){
+if (numpass >= 0){
+    console.log('numpass');
+    else
+    console.error("The number of passengers must be a positive integer value");
 }
 
+if (flightcapacity >= 0){
+    console.log('flightcapacity');
+    else
+    console.error("The capacity of the flight must be a positive integer value");
+}
+}
+let x = dislim;
+let y = disarr;
+let tot = x + y;
+function checkAircraftRevision(x,y){
+ if (tot<=(x/2)){
+    console.log("The revision needs to be done within the next 3 months")
+ }
+ else if(tot<=(3*x)){
+    console.log("The revision needs to be done within the next 2 months")
+ }
+ else if(tot>(3*x) && (tot<=x)){
+    console.log("The revision needs to be done within the next month")
+ }
+ else (tot>x){
+    console.error();
+ }
+}
+}
+return {calculateNumberOfFlights, checkAircraftRevision};
+module.exports = Flights();
+
+let a = flicap;
+let b = pass;
+let c = b .length;
+function Passengers(){
+    function checkFlightCapacity(a,b){
+if (c<a){
+    return(c);
+    else if (c>a){
+        console.error();
+    }
+}
+    }
+let vip = vip;
+let reg = regularpass;
+let nofli = noofflights;
+let bussi = bussinessseat;
+let eco = economyseats;
+
+  function  distributeAllSeatsToAllPassengers(vip, reg, nofli, bussi, eco){
+    if (vip = bussi){
+        console.log('full');
+        else if (vip> bussi) 
+    }
+  }
+    module.exports = Passengers();
+}
